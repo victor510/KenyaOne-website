c83e6b955f111dae610121dd1da4ef49ba188591
Victor510 <babyinfant794@gmail.com>
2021-12-17 16:05:59 +0300

    major change : -- Addition of GSAP CDN --Added main overlay -- menu btn animation | <a> tags -- media query | sub-section-main-content | margin-left

--- Karibu Kenya/Assets/Js/app.js
+++ Karibu Kenya/Assets/Js/app.js
@@ -1,9 +1,11 @@
 const moreFactsBtn = document.getElementById('see-more');
 const quickFactsSection = document.getElementById('quick-facts');
 const navContainer = document.getElementById('nav-container');
+const mainOverlay = document.getElementById('main_overlay');
 const menuBtn = document.getElementById('menu_btn');
 const closeBtn = document.getElementById('close_btn');
 const closeBtn_QuickFacts = document.getElementById('quick-facts close_btn');
+
 
 
 function toggle(el,classname){
@@ -17,19 +19,42 @@ function toggle(el,classname){
 
 moreFactsBtn.addEventListener('click', (e)=>{
     e.preventDefault();
-    toggle(quickFactsSection, 'active')
+    toggle(quickFactsSection, 'active');
+    toggle(mainOverlay, 'active')
 })
 closeBtn_QuickFacts.addEventListener('click', (e)=>{
     e.preventDefault();
-    toggle(quickFactsSection, 'active')
+    toggle(quickFactsSection, 'active');
+    toggle(mainOverlay, 'active')
 })
 
 menu_btn.addEventListener('click', (e)=>{
     e.preventDefault();
-    toggle(navContainer, 'active')
+    gsap.to('.nav-container', {
+        duration: .8,
+        width: '100%',
+        visibility: 'visible'
+    })
+    gsap.from('.close_btn', {
+        delay: .6,
+        duration: .2,
+        opacity: 0
+    })
+    gsap.to('.nav-link', {
+        opacity: 1,
+        stagger: .15
+    })
 })
 
 closeBtn.addEventListener('click', (e)=>{
     e.preventDefault();
-    toggle(navContainer, 'active')
+    gsap.to('.nav-container', {
+        duration: .8,
+        visibility: 'hidden',
+        width: '0'
+    })
+    gsap.to('.nav-link', {
+        opacity: 0,
+        stagger: .15
+    })
 })
